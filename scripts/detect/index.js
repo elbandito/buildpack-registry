6a6061a1016b056fbf433a30539b2de11df62193
Travis <longoria.public@gmail.com>
2020-07-13 13:46:33 -0600

    stable

--- scripts/detect/index.js
+++ scripts/detect/index.js
@@ -1,14 +1,31 @@
-module.exports = ({context}) => {
-    const test = `
+const toml = require('toml');
+
+const testIssue = `
 id = "projectriff/node-function"
 version = "0.6.2"
 addr = "gcr.io/projectriff/node-function@sha256:9d88250dfd77dbf5a535f1358c6a05dc2c0d3a22defbdcd72bb8f5e24b84e21d"
 `
 
-    let body = context.payload.issue.body
-    body = body.replace(/\r?\n|\r/, "");
+function validateIssue({context}) {
+    if (context.payload.issue.title === "") {
+        return new Error("issue title is missing")
+    }
 
-    let toml = require('toml');
-    let data = toml.parse(body);
-    return body;
-  }
+    if (!context.payload.issue.title.includes('ADD')) {
+        return new Error("issue should contain ADD")
+    }
+
+    const cleanBody = (context.payload.issue.body).replace(/\r?\n|\r/, "")
+    let tomlData
+    try {
+        tomlData = toml.parse(cleanBody)
+    } catch (err) {
+        return new Error("issue with TOML: " + err.message)
+    }
+
+    return tomlData
+}
+
+module.exports = {
+    validateIssue
+}
