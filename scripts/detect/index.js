ebe1e70d1c79205b2f774385a73f77933479cc4b
Travis <longoria.public@gmail.com>
2020-07-13 20:38:27 -0600

    more validation and clean object

--- scripts/detect/index.js
+++ scripts/detect/index.js
@@ -1,29 +1,55 @@
-const toml = require('toml');
+const Schema = require('validate')
+const Toml = require('toml')
 
 const testIssue = `
 id = "projectriff/node-function"
 version = "0.6.2"
 addr = "gcr.io/projectriff/node-function@sha256:9d88250dfd77dbf5a535f1358c6a05dc2c0d3a22defbdcd72bb8f5e24b84e21d"
 `
+const bodySchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    version: {
+        type: String,
+        required: true
+    },
+    addr: {
+        type: String,
+        match: /[\w][\w.-]{0,127}/,
+        required: true
+    },
+})
 
 function validateIssue({context}) {
     if (context.payload.issue.title === "") {
-        return new Error("issue title is missing")
+        throw new Error("issue title is missing")
     }
-
     if (!context.payload.issue.title.includes('ADD')) {
-        return new Error("issue should contain ADD")
+        throw new Error("issue should contain ADD")
     }
 
     const cleanBody = (context.payload.issue.body).replace(/\r?\n|\r/, "")
     let tomlData
     try {
-        tomlData = toml.parse(cleanBody)
+        tomlData = Toml.parse(cleanBody)
     } catch (err) {
-        return new Error("issue with TOML: " + err.message)
+        throw new Error(`issue with TOML: ${err.message}`)
     }
 
-    return tomlData
+    const errors = bodySchema.validate(tomlData)
+    if (errors && errors.length > 0) {
+        throw new Error(`invalid issue body: ${errors}`)
+    }
+
+    return {
+        ns: tomlData.id.split("/")[0],
+        name: tomlData.id.split("/")[1],
+        version: tomlData.version,
+        yanked: false,
+        addr: tomlData.addr
+    }
 }
 
 module.exports = {
